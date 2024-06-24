81bfa4174db931f990d0205004a8f80b81a1e0d9
IvGomes <ivan.kyogomes@gmail.com>
2024-06-24 19:32:41 -0300

    fix: conflict

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,8 @@
 import Header from './components/header';
 import TopFooter from './components/TopFooter';
 import Footer from './components/footer/Footer';
-<<<<<<< HEAD
 import { DepositionsSection } from './components/depositions/DepositionsSection';
-=======
 import TabsSection from './components/tabs-section';
->>>>>>> 4afa362783200c7ee2ded69a0995396cbf500a3c
 
 export default function Home() {
   return (
@@ -16,11 +13,8 @@ export default function Home() {
         <p className="mt-4 text-lg">Sua fábrica de software de confiança</p>
         <TabsSection />
       </main>
-<<<<<<< HEAD
       <DepositionsSection />
-=======
       <TopFooter />
->>>>>>> 4afa362783200c7ee2ded69a0995396cbf500a3c
       <Footer />
       
     </div>
