4afa362783200c7ee2ded69a0995396cbf500a3c
Samuel Oliveira <samuelzinS10@gmail.com>
2024-06-24 18:41:18 -0300

    feat:starting the tabs-section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Header from './components/header';
 import TopFooter from './components/TopFooter';
 import Footer from './components/footer/Footer';
+import TabsSection from './components/tabs-section';
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <main className="min-h-screen flex flex-col justify-center items-center">
         <h1 className="text-4xl font-bold">Bem-vindo à S4STECHLP</h1>
         <p className="mt-4 text-lg">Sua fábrica de software de confiança</p>
+        <TabsSection />
       </main>
       <TopFooter />
       <Footer />
