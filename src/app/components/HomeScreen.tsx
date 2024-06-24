66462e9dde577a335420e8903fab1ff3eeb89f08
Seu Novo Nome de Usuário <seu-novo-email@example.com>
2024-06-24 18:19:55 -0300

    homescreen

---
+++ src/app/components/HomeScreen.tsx
@@ -0,0 +1,13 @@
+export default function HomeScreen(){
+    return (
+        <div className="pt-20">
+            <div className="border-[1px] border-[#222222] rounded-full pt-[16px] pb-[16px] pl-[8px] text-center">
+                <span className="text-base/[16px] text-center font-semibold text-[#1F1F1F] bg-[#E19322] rounded-full pt-[12px] pb-[12px] pl-[34px] pr-[34px]">Olá!</span>
+                <span className="text-base/[16px] text-center font-normal text-[#FFFFFF] ml-6">No que podemos ajudar?</span>
+            </div>
+            <div>
+                <h1>Transforme sua idea em realidade.</h1>
+            </div>
+        </div>
+    );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Header from './components/header';
 import Footer from './components/Footer';
+import HomeScreen from './components/HomeScreen';
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <HomeScreen />
       <main className="min-h-screen flex flex-col justify-center items-center">
         <h1 className="text-4xl font-bold">Bem-vindo à S4STECHLP</h1>
         <p className="mt-4 text-lg">Sua fábrica de software de confiança</p>
