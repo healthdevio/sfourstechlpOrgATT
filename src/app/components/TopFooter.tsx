52525dcf6188b2958be075d587cd66cdf521f98e
nicolas ♺ <odevnicolas@gmail.com>
2024-06-24 18:26:41 -0300

    ajustando a cor do TopFooter

--- src/app/components/TopFooter.tsx
+++ src/app/components/TopFooter.tsx
@@ -16,11 +16,11 @@ export default  function TopFooter() {
                 <div className=" flex   justify-between ">
                     <div className=" flex gap-2 ml-24 " >
                         <input type="email" name="" id="" className=" mt-10 ml-10 w-72 h-12 bg-zinc-800 text-white rounded-sm p-2  text-sm " placeholder="Entre com o seu endereço de e-mail"/>
-                         <button className=" mt-10  h-12  w-36 size-4 font-semibold  bg-yellow-600 text-black rounded-sm  text-sm " >Enviar</button>
+                         <button className=" mt-10  h-12  w-36 size-4 font-semibold  bg-[#AD6600] text-black rounded-sm  text-sm " >Enviar</button>
                     </div>
                    
                     <div >
-                        <button className=" mt-8  mr-64  h-10  w-36 size-4 font-semibold  bg-yellow-600 text-black rounded-sm  text-sm border  border-red-900" >Conhecer</button>
+                        <button className=" mt-8  mr-64  h-10  w-36 size-4 font-semibold  bg-[#AD6600] text-black rounded-sm  text-sm border  border-red-900" >Conhecer</button>
                     </div>
                 </div>
             
