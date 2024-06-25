065b8114ee1179b45617975682cb5445c7005639
ERIK BARBOSA <erikbarbosa5283@gmail.com>
2024-06-25 14:21:29 -0300

    feat: alteraçao no Topfooter no imput e no texto

--- src/app/components/TopFooter.tsx
+++ src/app/components/TopFooter.tsx
@@ -4,8 +4,9 @@ export default  function TopFooter() {
 
                 <div className=" flex   justify-between ml-24 ">
                     <div className=" gap-2 " >
-                    <h1 className=" font-bold  size-32  text-4xl w-auto pt-20 pl-10 space">Venha fazer parte do  <br/> nosso time. </h1> <br/>
-                    <h4 className=" mt-10  ml-10 opacity-40 "  >informe o seu e-mail para receber noticias de futuras vagas. </h4>
+                    <h1 className=" font-bold  size-32  text-4xl w-auto pt-20 pl-10   leading-normal">Venha fazer parte do  <br/> nosso time. </h1>
+                     <br/>
+                    <h4 className=" mt-12  ml-10 opacity-40 "  >informe o seu e-mail para receber noticias de futuras vagas. </h4>
                     </div>
                    
                     <div >
@@ -15,12 +16,12 @@ export default  function TopFooter() {
 
                 <div className=" flex   justify-between ">
                     <div className=" flex gap-2 ml-24 " >
-                        <input type="email" name="" id="" className=" mt-10 ml-10 w-72 h-12 bg-zinc-800 text-white rounded-sm p-2  text-sm " placeholder="Entre com o seu endereço de e-mail"/>
+                        <input type="email" name="" id="" className=" mt-10   ml-10  w-96 h-12 bg-zinc-800 text-white rounded-sm  p-6  text-xs " placeholder="Entre com o seu endereço de e-mail"/>
                          <button className=" mt-10  h-12  w-36 size-4 font-semibold  bg-yellow-600 text-black rounded-sm  text-sm " >Enviar</button>
                     </div>
                    
                     <div >
-                        <button className=" mt-8  mr-64  h-10  w-36 size-4 font-semibold  bg-yellow-600 text-black rounded-sm  text-sm border  border-red-900" >Conhecer</button>
+                        <button className=" mt-8  mr-64  h-10  w-36 size-4 font-semibold  bg-yellow-600 text-black rounded-sm  text-sm " >Conhecer</button>
                     </div>
                 </div>
             
