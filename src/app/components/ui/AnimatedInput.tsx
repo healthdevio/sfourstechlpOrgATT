e1c0b0e7df476e781a73a0a3807a4d220353c3c2
“odevnicolas” <nickolas@s4s.tech>
2024-07-01 15:17:52 -0300

    mudanca cores

--- src/app/components/ui/AnimatedInput.tsx
+++ src/app/components/ui/AnimatedInput.tsx
@@ -20,7 +20,7 @@ export const AnimatedInput = () => {
       <input
         type="text"
         placeholder="Em que podemos ajudar?"
-        className="peer pl-20 focus:pl-4 pr-4 focus:pr-20 py-2 border border-gray-300 rounded-full w-full focus:outline-none focus:border-blue-500 transition-all bg-transparent"
+        className="peer pl-20 focus:pl-4 pr-4 focus:pr-20 py-2 border border-[#E19322] rounded-full w-full focus:outline-none focus:border-[#E19322] transition-all bg-transparent"
         onFocus={handleFocus}
         onBlur={handleBlur}
         onChange={handleChange}
@@ -29,7 +29,7 @@ export const AnimatedInput = () => {
       <Link
         href={`https://wa.me/?text=${encodeURIComponent(message)}`}
         target='_blank'
-        className={`absolute flex items-center left-1 peer-focus:left-auto peer-focus:right-1 py-1 px-5 top-1/2 transform -translate-y-1/2 transition-all text-base h-[calc(100%-5px)] text-[#1F1F1F] font-semibold rounded-full bg-[#E19322] peer-focus:bg-blue-500 peer-focus:text-white`}
+        className={`absolute flex items-center left-1 peer-focus:left-auto peer-focus:right-1 py-1 px-5 top-1/2 transform -translate-y-1/2 transition-all text-base h-[calc(100%-5px)] text-[#1F1F1F] font-semibold rounded-full bg-[#E19322] peer-focus:bg-[#E19322] peer-focus:text-[#1F1F1F]`}
       >
         <span className='-z-10'>
           {isFocused ? <Send size={15} /> : 'Olá'}
