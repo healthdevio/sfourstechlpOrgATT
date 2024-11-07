dbe39607d186dab03458669f4e7bdd9c7d538925
xXNextGenXx <mundocurioso3294@gmail.com>
2024-11-07 15:40:11 -0300

    ajustes rápidos no em que podemos ajudar

--- src/app/components/ui/AnimatedInput.tsx
+++ src/app/components/ui/AnimatedInput.tsx
@@ -1,7 +1,5 @@
-// components/AnimatedInput.tsx
 import { useState } from 'react';
 import { Send } from 'lucide-react';
-import Link from 'next/link';
 
 export const AnimatedInput = () => {
   const [isFocused, setIsFocused] = useState(false);
@@ -10,10 +8,6 @@ export const AnimatedInput = () => {
   const handleFocus = () => setIsFocused(true);
   const handleBlur = () => setIsFocused(false);
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => setMessage(e.target.value);
-  const handleSubmit = () => {
-    const whatsappUrl = `https://wa.me/558588778023?text=${encodeURIComponent(message)}`;
-    window.open(whatsappUrl, '_blank');
-  };
 
   return (
     <div className="relative flex items-center w-full">
@@ -26,15 +20,17 @@ export const AnimatedInput = () => {
         onChange={handleChange}
         value={message}
       />
-      <Link
-        href={`https://wa.me/?text=${encodeURIComponent(message)}`}
-        target='_blank'
-        className={`absolute flex items-center left-1 peer-focus:left-auto peer-focus:right-1 py-1 px-5 top-1/2 transform -translate-y-1/2 transition-all text-base h-[calc(100%-5px)] text-[#1F1F1F] font-semibold rounded-full bg-[#E19322] peer-focus:bg-[#E19322] peer-focus:text-[#1F1F1F]`}
+
+      <a
+        href={`https://wa.me/558588778023?text=${encodeURIComponent(message)}`}
+        target="_blank"
+        onMouseDown={(e) => e.preventDefault()} 
+        className="absolute flex items-center left-1 peer-focus:left-auto peer-focus:right-1 py-1 px-5 top-1/2 transform -translate-y-1/2 transition-all text-base h-[calc(100%-5px)] text-[#1F1F1F] font-semibold rounded-full bg-[#E19322] peer-focus:bg-[#E19322] peer-focus:text-[#1F1F1F]"
       >
-        <span className='-z-10'>
+        <span className="-z-10">
           {isFocused ? <Send size={15} /> : 'Olá'}
         </span>
-      </Link>
+      </a>
     </div>
   );
 };
